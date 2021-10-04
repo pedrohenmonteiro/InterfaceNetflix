e9ee2f604bfc9b71685aa2eaf80af014431bb430
Pedro Monteiro <pedromont436@gmail.com>
2021-10-03 23:14:29 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        500:{
+            items:3
+        },
+        
+
+        600:{
+            items:4
+        },
+        
+
+        800:{
+            items:5
+        },
+
+
+        1000:{
+            items:6
+        },
+
+        1200:{
+            items:7
+        },
+
+    }
+})
